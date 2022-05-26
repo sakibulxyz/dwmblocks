257cbdda669987f77b91c35ae995d9943d503087
sakibul <sakibul706@gmail.com>
2022-05-26 10:44:18 +0600

    [this bracket looks good]

--- config.h
+++ config.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {" ",  "st-nettraf",  1,  0},
-  {" ",  "st-internet",  10,  0},
-  {"",  "st-battary",  10,  0},
-  {"",  "st-volume", 3,  0},
-  {"",  "st-clock",  59, 0},
+  {" [ ",  "st-nettraf",  1,  0},
+  {"] [ ",  "st-internet",  10,  0},
+  {"] [ ",  "st-battary",  10,  0},
+  {"] [ ",  "st-volume", 3,  0},
+  {"] ",  "st-clock",  59, 0},
 
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "";
 static unsigned int delimLen = 5;
